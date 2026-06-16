1654c5b146342fb90859ba12050e32b971505c3d
KARTHI2509 <karthikthalipineni@gmail.com>
2026-06-17 00:55:39 +0530

    Added complete SQL dump

---
+++ full_project.sql
@@ -0,0 +1,459 @@
+-- MySQL dump 10.13  Distrib 8.0.46, for Win64 (x86_64)
+--
+-- Host: localhost    Database: projectsql
+-- ------------------------------------------------------
+-- Server version	8.0.46
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `applications`
+--
+
+DROP TABLE IF EXISTS `applications`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `applications` (
+  `application_id` int NOT NULL,
+  `student_id` int DEFAULT NULL,
+  `job_id` int DEFAULT NULL,
+  `status` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`application_id`),
+  KEY `student_id` (`student_id`),
+  KEY `job_id` (`job_id`),
+  CONSTRAINT `applications_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `students` (`student_id`),
+  CONSTRAINT `applications_ibfk_2` FOREIGN KEY (`job_id`) REFERENCES `jobs` (`job_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `applications`
+--
+
+LOCK TABLES `applications` WRITE;
+/*!40000 ALTER TABLE `applications` DISABLE KEYS */;
+INSERT INTO `applications` VALUES (1,1,1,'Applied'),(2,1,5,'Applied'),(3,2,3,'Applied'),(4,3,1,'Applied'),(5,4,2,'Applied'),(6,5,3,'Applied'),(7,6,4,'Applied'),(8,7,5,'Applied'),(9,8,2,'Applied'),(10,10,3,'Applied'),(12,6,4,'Applied');
+/*!40000 ALTER TABLE `applications` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = cp850 */ ;
+/*!50003 SET character_set_results = cp850 */ ;
+/*!50003 SET collation_connection  = cp850_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `auto_status` BEFORE INSERT ON `applications` FOR EACH ROW BEGIN
+    SET NEW.status = 'Applied';
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Table structure for table `aptitude_tests`
+--
+
+DROP TABLE IF EXISTS `aptitude_tests`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `aptitude_tests` (
+  `test_id` int NOT NULL,
+  `student_id` int DEFAULT NULL,
+  `marks` int DEFAULT NULL,
+  PRIMARY KEY (`test_id`),
+  KEY `student_id` (`student_id`),
+  CONSTRAINT `aptitude_tests_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `students` (`student_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `aptitude_tests`
+--
+
+LOCK TABLES `aptitude_tests` WRITE;
+/*!40000 ALTER TABLE `aptitude_tests` DISABLE KEYS */;
+INSERT INTO `aptitude_tests` VALUES (1,1,85),(2,2,92),(3,3,70),(4,4,95),(5,5,80),(6,6,65),(7,7,98),(8,8,75),(9,9,68),(10,10,88);
+/*!40000 ALTER TABLE `aptitude_tests` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `companies`
+--
+
+DROP TABLE IF EXISTS `companies`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `companies` (
+  `company_id` int NOT NULL,
+  `company_name` varchar(100) DEFAULT NULL,
+  `location` varchar(100) DEFAULT NULL,
+  `package_lpa` decimal(4,2) DEFAULT NULL,
+  PRIMARY KEY (`company_id`),
+  UNIQUE KEY `company_name` (`company_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `companies`
+--
+
+LOCK TABLES `companies` WRITE;
+/*!40000 ALTER TABLE `companies` DISABLE KEYS */;
+INSERT INTO `companies` VALUES (1,'TCS','Hyderabad',4.50),(2,'Infosys','Bangalore',5.20),(3,'Accenture','Chennai',6.00),(4,'Wipro','Pune',4.20),(5,'Cognizant','Hyderabad',5.80);
+/*!40000 ALTER TABLE `companies` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `departments`
+--
+
+DROP TABLE IF EXISTS `departments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `departments` (
+  `dept_id` int NOT NULL,
+  `dept_name` varchar(50) NOT NULL,
+  PRIMARY KEY (`dept_id`),
+  UNIQUE KEY `dept_name` (`dept_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `departments`
+--
+
+LOCK TABLES `departments` WRITE;
+/*!40000 ALTER TABLE `departments` DISABLE KEYS */;
+INSERT INTO `departments` VALUES (105,'Civi'),(101,'CSE'),(102,'ECE'),(103,'EEE'),(104,'Mechanical');
+/*!40000 ALTER TABLE `departments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Temporary view structure for view `high_cgpa_students`
+--
+
+DROP TABLE IF EXISTS `high_cgpa_students`;
+/*!50001 DROP VIEW IF EXISTS `high_cgpa_students`*/;
+SET @saved_cs_client     = @@character_set_client;
+/*!50503 SET character_set_client = utf8mb4 */;
+/*!50001 CREATE VIEW `high_cgpa_students` AS SELECT 
+ 1 AS `student_id`,
+ 1 AS `student_name`,
+ 1 AS `email`,
+ 1 AS `phone`,
+ 1 AS `cgpa`,
+ 1 AS `dept_id`*/;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Table structure for table `interviews`
+--
+
+DROP TABLE IF EXISTS `interviews`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `interviews` (
+  `interview_id` int NOT NULL,
+  `student_id` int DEFAULT NULL,
+  `company_id` int DEFAULT NULL,
+  `round_name` varchar(50) DEFAULT NULL,
+  `result` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`interview_id`),
+  KEY `student_id` (`student_id`),
+  KEY `company_id` (`company_id`),
+  CONSTRAINT `interviews_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `students` (`student_id`),
+  CONSTRAINT `interviews_ibfk_2` FOREIGN KEY (`company_id`) REFERENCES `companies` (`company_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `interviews`
+--
+
+LOCK TABLES `interviews` WRITE;
+/*!40000 ALTER TABLE `interviews` DISABLE KEYS */;
+INSERT INTO `interviews` VALUES (1,1,1,'Technical','Pass'),(2,2,3,'Technical','Pass'),(3,3,1,'Technical','Fail'),(4,4,2,'Technical','Pass'),(5,5,5,'Technical','Pass'),(6,6,4,'Technical','Fail'),(7,7,5,'Technical','Pass'),(8,8,2,'Technical','Pass'),(9,10,3,'Technical','Pass');
+/*!40000 ALTER TABLE `interviews` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `jobs`
+--
+
+DROP TABLE IF EXISTS `jobs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `jobs` (
+  `job_id` int NOT NULL,
+  `company_id` int DEFAULT NULL,
+  `role_name` varchar(100) DEFAULT NULL,
+  `min_cgpa` decimal(3,2) DEFAULT NULL,
+  PRIMARY KEY (`job_id`),
+  KEY `company_id` (`company_id`),
+  CONSTRAINT `jobs_ibfk_1` FOREIGN KEY (`company_id`) REFERENCES `companies` (`company_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `jobs`
+--
+
+LOCK TABLES `jobs` WRITE;
+/*!40000 ALTER TABLE `jobs` DISABLE KEYS */;
+INSERT INTO `jobs` VALUES (1,1,'Java Developer',7.50),(2,2,'Python Developer',8.00),(3,3,'Full Stack Developer',8.20),(4,4,'Data Analyst',7.00),(5,5,'Software Engineer',7.80);
+/*!40000 ALTER TABLE `jobs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `placements`
+--
+
+DROP TABLE IF EXISTS `placements`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `placements` (
+  `placement_id` int NOT NULL,
+  `student_id` int DEFAULT NULL,
+  `company_id` int DEFAULT NULL,
+  `package_lpa` decimal(4,2) DEFAULT NULL,
+  PRIMARY KEY (`placement_id`),
+  KEY `student_id` (`student_id`),
+  KEY `company_id` (`company_id`),
+  CONSTRAINT `placements_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `students` (`student_id`),
+  CONSTRAINT `placements_ibfk_2` FOREIGN KEY (`company_id`) REFERENCES `companies` (`company_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `placements`
+--
+
+LOCK TABLES `placements` WRITE;
+/*!40000 ALTER TABLE `placements` DISABLE KEYS */;
+INSERT INTO `placements` VALUES (1,1,1,4.50),(2,2,3,6.00),(3,4,2,5.20),(4,5,5,5.80),(5,7,5,5.80),(6,8,2,5.20),(7,10,3,6.00);
+/*!40000 ALTER TABLE `placements` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Temporary view structure for view `selected_students`
+--
+
+DROP TABLE IF EXISTS `selected_students`;
+/*!50001 DROP VIEW IF EXISTS `selected_students`*/;
+SET @saved_cs_client     = @@character_set_client;
+/*!50503 SET character_set_client = utf8mb4 */;
+/*!50001 CREATE VIEW `selected_students` AS SELECT 
+ 1 AS `student_name`,
+ 1 AS `company_name`,
+ 1 AS `package_lpa`*/;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Table structure for table `skills`
+--
+
+DROP TABLE IF EXISTS `skills`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `skills` (
+  `skill_id` int NOT NULL,
+  `skill_name` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`skill_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `skills`
+--
+
+LOCK TABLES `skills` WRITE;
+/*!40000 ALTER TABLE `skills` DISABLE KEYS */;
+INSERT INTO `skills` VALUES (1,'Java'),(2,'Python'),(3,'SQL'),(4,'React'),(5,'Spring Boot');
+/*!40000 ALTER TABLE `skills` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `student_backup`
+--
+
+DROP TABLE IF EXISTS `student_backup`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `student_backup` (
+  `student_id` int DEFAULT NULL,
+  `student_name` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `phone` varchar(15) DEFAULT NULL,
+  `cgpa` decimal(3,2) DEFAULT NULL,
+  `dept_id` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `student_backup`
+--
+
+LOCK TABLES `student_backup` WRITE;
+/*!40000 ALTER TABLE `student_backup` DISABLE KEYS */;
+/*!40000 ALTER TABLE `student_backup` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `student_skills`
+--
+
+DROP TABLE IF EXISTS `student_skills`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `student_skills` (
+  `student_id` int NOT NULL,
+  `skill_id` int NOT NULL,
+  PRIMARY KEY (`student_id`,`skill_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `student_skills`
+--
+
+LOCK TABLES `student_skills` WRITE;
+/*!40000 ALTER TABLE `student_skills` DISABLE KEYS */;
+INSERT INTO `student_skills` VALUES (1,1),(1,3),(2,2),(2,4),(3,1),(4,2),(5,5),(7,1),(7,5),(10,3);
+/*!40000 ALTER TABLE `student_skills` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `students`
+--
+
+DROP TABLE IF EXISTS `students`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `students` (
+  `student_id` int NOT NULL AUTO_INCREMENT,
+  `student_name` varchar(100) NOT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `phone` varchar(15) DEFAULT NULL,
+  `cgpa` decimal(3,2) DEFAULT NULL,
+  `dept_id` int DEFAULT NULL,
+  PRIMARY KEY (`student_id`),
+  UNIQUE KEY `email` (`email`),
+  KEY `dept_id` (`dept_id`),
+  CONSTRAINT `students_ibfk_1` FOREIGN KEY (`dept_id`) REFERENCES `departments` (`dept_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `students`
+--
+
+LOCK TABLES `students` WRITE;
+/*!40000 ALTER TABLE `students` DISABLE KEYS */;
+INSERT INTO `students` VALUES (1,'Saran','saran@gmail.com','9876543210',9.20,101),(2,'Priya','priya@gmail.com','9876543211',9.00,102),(3,'Rahul','rahul@gmail.com','9876543212',7.80,101),(4,'Deepa','deepa@gmail.com','9876543213',8.90,103),(5,'Anjali','anjali@gmail.com','9876543214',8.20,102),(6,'Kumar','kumar@gmail.com','9876543215',7.50,104),(7,'Sneha','sneha@gmail.com','9876543216',9.10,101),(8,'Varun','varun@gmail.com','9876543217',8.00,105),(9,'Charan','charan@gmail.com','9876543218',7.20,104),(10,'Harsha','harsha@gmail.com','9876543219',8.70,101),(11,'Ramesh','ramesh@gmail.com','9876543299',8.60,101);
+/*!40000 ALTER TABLE `students` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = cp850 */ ;
+/*!50003 SET character_set_results = cp850 */ ;
+/*!50003 SET collation_connection  = cp850_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `check_student_cgpa` BEFORE INSERT ON `students` FOR EACH ROW BEGIN
+    IF NEW.cgpa < 6 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'CGPA below 6 not allowed';
+    END IF;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = cp850 */ ;
+/*!50003 SET character_set_results = cp850 */ ;
+/*!50003 SET collation_connection  = cp850_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `backup_deleted_students` BEFORE DELETE ON `students` FOR EACH ROW BEGIN
+    INSERT INTO student_backup
+    VALUES(
+        OLD.student_id,
+        OLD.student_name,
+        OLD.email,
+        OLD.phone,
+        OLD.cgpa,
+        OLD.dept_id
+    );
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Final view structure for view `high_cgpa_students`
+--
+
+/*!50001 DROP VIEW IF EXISTS `high_cgpa_students`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = cp850 */;
+/*!50001 SET character_set_results     = cp850 */;
+/*!50001 SET collation_connection      = cp850_general_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `high_cgpa_students` AS select `students`.`student_id` AS `student_id`,`students`.`student_name` AS `student_name`,`students`.`email` AS `email`,`students`.`phone` AS `phone`,`students`.`cgpa` AS `cgpa`,`students`.`dept_id` AS `dept_id` from `students` where (`students`.`cgpa` > 8) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+
+--
+-- Final view structure for view `selected_students`
+--
+
+/*!50001 DROP VIEW IF EXISTS `selected_students`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = cp850 */;
+/*!50001 SET character_set_results     = cp850 */;
+/*!50001 SET collation_connection      = cp850_general_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `selected_students` AS select `s`.`student_name` AS `student_name`,`c`.`company_name` AS `company_name`,`p`.`package_lpa` AS `package_lpa` from ((`placements` `p` join `students` `s` on((`p`.`student_id` = `s`.`student_id`))) join `companies` `c` on((`p`.`company_id` = `c`.`company_id`))) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-06-17  0:55:20
